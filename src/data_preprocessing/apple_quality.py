374e3c29de35109d3158b1f5eb43843b103a9414
MichalTaczala <michtaczala@gmail.com>
2024-03-27 19:05:19 +0100

    add data loading

--- src/data_preprocessing/apple_quality.py
+++ src/data_preprocessing/apple_quality.py
@@ -1,22 +1,35 @@
 import torch
 
-from .base import PreprocessData
+from base import PreprocessData
+
+import pandas as pd
+import os
 
 
 class AppleQuality(PreprocessData):
     def __init__(self, data_dir: str):
         super().__init__(
             data_dir,
-            "Apple Quality",
+            "AppleQuality",
             """This dataset contains information about various 
-            attributes of a set of fruits, providing insights into their characteristics."""
-            ,
+            attributes of a set of fruits, providing insights into their characteristics.""",
         )
 
     def load_and_transform(self) -> tuple[torch.Tensor, torch.Tensor]:
-        pass
+        df = pd.read_csv(os.path.join(self.data_dir, "apple_quality.csv"))
+        df = df.drop(df.index[-1])
+        df = df.drop(columns=df.columns[0])
+        X, y = df.drop(columns=["Quality"]), df["Quality"]
+        y = y.map({"good": 1, "bad": 0})
+        X = X.astype("float32")
+        X = torch.tensor(X.values, dtype=torch.float32)
+        y = torch.tensor(y.values, dtype=torch.int)
+        return X, y
 
 
 if __name__ == "__main__":
-    apple_quality = AppleQuality("")
+    current_dir = os.getcwd()
+    print("Current directory:", current_dir)
+    apple_quality = AppleQuality("data")
+    apple_quality.load_and_transform()
     apple_quality.upload_data()
