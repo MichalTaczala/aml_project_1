0f5cf5015d73ddf679a2e294434e927298b36daf
MichalTaczala <michtaczala@gmail.com>
2024-03-27 17:37:59 +0100

    change base params

--- src/optimizers/base.py
+++ src/optimizers/base.py
@@ -9,23 +9,21 @@ class Base(ABC):
         self.lr = lr
 
     def beta_1_grad(
-            self, X: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor
+        self, X: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor
     ) -> torch.Tensor:
         return torch.mean(X.T * (y - y_hat), dim=1)
 
-    def beta_0_grad(
-            self, y: torch.Tensor, y_hat: torch.Tensor
-    ) -> torch.Tensor:
-        return torch.mean(y-y_hat)
+    def beta_0_grad(self, y: torch.Tensor, y_hat: torch.Tensor) -> torch.Tensor:
+        return torch.mean(y - y_hat)
 
     def grad(
-            self, X: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor
+        self, X: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
         return self.beta_0_grad(y, y_hat), self.beta_1_grad(X, y, y_hat)
 
-    def step(self, grad: torch.Tensor) -> torch.Tensor:
+    def step(self, grad: torch.Tensor, param_name=None) -> torch.Tensor:
         raise NotImplementedError
 
     def backprop(self, X: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor):
-        self.model.beta0 += self.step(self.beta_0_grad(y, y_hat))
-        self.model.beta1 += self.step(self.beta_1_grad(X, y, y_hat))
+        self.model.beta0 += self.step(self.beta_0_grad(y, y_hat), "beta0")
+        self.model.beta1 += self.step(self.beta_1_grad(X, y, y_hat), "beta1")
